c7534b25be25a9534524bb7d6280e42f1b3f62d5
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-08 15:09:55 -0500

    first

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-# README for a newly created project.
+leaflet-ajax
+===========
 
-There are a couple of things you should do first, before you can use all of Git's power:
+alows you to call json over ajax either localally or with jsonp, for local json the api is pretty much the same
 
-  * Add a remote to this project: in the Cloud9 IDE command line, you can execute the following commands
-    `git remote add [remote name] [remote url (eg. 'git@github.com:/ajaxorg/node_chat')]` [Enter]
-  * Create new files inside your project
-  * Add them to to Git by executing the following command
-    `git add [file1, file2, file3, ...]` [Enter]
-  * Create a commit which can be pushed to the remote you just added
-    `git commit -m 'added new files'` [Enter]
-  * Push the commit the remote
-    `git push [remote name] master` [Enter]
+```js
+var geojsonLayer = new L.GeoJSON.AJAX("geojson.json");
+```
+for jsonp add the option "dataType" and set it to "jsonp"
+```js
+var geojsonLayer = new L.GeoJSON.AJAX("http:webhost.fake/geojson.jsonp",{dataType:"jsonp"});
+```
+for weirder jsonp there are the other two new options "callbackName" for if you need to specify the name of the callback and "callbackParam" for if you need to change the name of the callback parameter to something besides "callback"
 
-That's it! If this doesn't work for you, please visit the excellent resources from [Github.com](http://help.github.com) and the [Pro Git](http://http://progit.org/book/) book.
-If you can't find your answers there, feel free to ask us via Twitter (@cloud9ide), [mailing list](groups.google.com/group/cloud9-ide) or IRC (#cloud9ide on freenode).
-
-Happy coding!
+some of the jsonp code inspired by/taken from [this interesting looking plugin](https://github.com/stefanocudini/leaflet-search) that I have failed to make heads nor tails of (the plugin, not the jsonp code)
---
+++ leaflet-ajax.js
@@ -0,0 +1,70 @@
+L.GeoJSON.AJAX=L.GeoJSON.extend({
+    defaultAJAXparams:{
+     dataType:"json",
+     callbackName:false,
+     callbackParam:"callback"
+    },
+    initialize: function (url, options) { // (String, Object)
+
+        this._url = url;
+        var ajaxParams = L.Util.extend({}, this.defaultAJAXparams);
+
+        for (var i in options) {
+    		 //all keys that are not GeoJSON options go to ajax params
+			if (this.defaultAJAXparams.hasOwnProperty(i)) {
+				ajaxParams[i] = options[i];
+			}
+		}
+
+		this.ajaxParams = ajaxParams;
+        this._layers = {};
+		L.Util.setOptions(this, options);
+        if(this._url){
+            this.addUrl(this._url);
+        }
+    },
+    addUrl: function (url) {
+        if(this.ajaxParams.dataType.toLowerCase()==="json"){
+           this.addJSON(url); 
+        }else if(this.ajaxParams.dataType.toLowerCase()==="jsonp"){
+            this.addJSONP(url); 
+        }
+    },
+    addJSON: function (url){
+        var response 
+        var request = new XMLHttpRequest();
+  
+        request.open("GET",url);
+        var _this=this;
+        request.onreadystatechange = function(){
+            if (request.readyState === 4 && request.status === 200 && request.getResponseHeader("Content-Type") === "application/json"){
+                response = JSON.parse(request.responseText);
+                _this.addData(response);
+            }
+        };
+        request.send();
+    },
+    addJSONP: function (url){
+        var _this=this,cbParam = this.ajaxParams.callbackParam,cbn,ourl;
+        if(this.ajaxParams.callbackName){
+            cbn= this.ajaxParams.callbackName;
+        }else{
+            cbn = "_" + Math.floor(Math.random()*1000000);
+        }
+        L.GeoJSON.AJAX.cb = {};
+        L.GeoJSON.AJAX.cb[cbn]=function(data){
+            _this.addData(JSON.parse(JSON.stringify(data)));
+        }
+        var scriptNode = L.DomUtil.create('script','', document.getElementsByTagName('body')[0] );
+        scriptNode.type = 'text/javascript';
+        if (url.indexOf("?") === -1 ){
+            ourl =  url+"?"+cbParam+"=L.GeoJSON.AJAX.cb."+cbn;
+        }else{
+            ourl =  url+"&"+cbParam+"=L.GeoJSON.AJAX.cb."+cbn;
+        }
+        scriptNode.src = ourl;    
+    }
+});
+L.geoJson.Ajax = function (geojson, options) {
+    return new L.GeoJSON.AJAX(geojson, options);
+};
