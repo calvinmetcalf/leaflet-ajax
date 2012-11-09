ad9f557d0f8f219b85994ca4f2afdb560a96f6be
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-09 08:54:05 -0500

    broke out the ajax into a seperate class

--- leaflet-ajax.js
+++ leaflet-ajax.js
@@ -1,7 +1,7 @@
+var L= this.L;
 L.GeoJSON.AJAX=L.GeoJSON.extend({
     defaultAJAXparams:{
      dataType:"json",
-     callbackName:false,
      callbackParam:"callback"
     },
     initialize: function (url, options) { // (String, Object)
@@ -10,7 +10,6 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
         var ajaxParams = L.Util.extend({}, this.defaultAJAXparams);
 
         for (var i in options) {
-    		 //all keys that are not GeoJSON options go to ajax params
 			if (this.defaultAJAXparams.hasOwnProperty(i)) {
 				ajaxParams[i] = options[i];
 			}
@@ -24,47 +23,45 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
         }
     },
     addUrl: function (url) {
+        var _this = this;
         if(this.ajaxParams.dataType.toLowerCase()==="json"){
-           this.addJSON(url); 
+          L.Util.ajax(url, function(data){_this.addData(data);}); 
         }else if(this.ajaxParams.dataType.toLowerCase()==="jsonp"){
-            this.addJSONP(url); 
+            L.Util.jsonp(url, function(data){_this.addData(data);}, _this.ajaxParams.callbackParam);
         }
-    },
-    addJSON: function (url){
-        var response 
-        var request = new XMLHttpRequest();
-  
-        request.open("GET",url);
-        var _this=this;
-        request.onreadystatechange = function(){
-            if (request.readyState === 4 && request.status === 200 ){
-                response = JSON.parse(request.responseText);
-                _this.addData(response);
-            }
-        };
-        request.send();
-    },
-    addJSONP: function (url){
-        var _this=this,cbParam = this.ajaxParams.callbackParam,cbn,ourl;
-        if(this.ajaxParams.callbackName){
-            cbn= this.ajaxParams.callbackName;
-        }else{
-            cbn = "_" + Math.floor(Math.random()*1000000);
-        }
-        L.GeoJSON.AJAX.cb = {};
-        L.GeoJSON.AJAX.cb[cbn]=function(data){
-            _this.addData(JSON.parse(JSON.stringify(data)));
-        }
-        var scriptNode = L.DomUtil.create('script','', document.getElementsByTagName('body')[0] );
-        scriptNode.type = 'text/javascript';
-        if (url.indexOf("?") === -1 ){
-            ourl =  url+"?"+cbParam+"=L.GeoJSON.AJAX.cb."+cbn;
-        }else{
-            ourl =  url+"&"+cbParam+"=L.GeoJSON.AJAX.cb."+cbn;
-        }
-        scriptNode.src = ourl;    
     }
 });
+L.Util.ajax = function (url, cb){
+    var response, request = new XMLHttpRequest();
+    request.open("GET",url);
+    request.onreadystatechange = function(){
+        if (request.readyState === 4 && request.status === 200 ){
+            response = JSON.parse(request.responseText);
+            cb(response);
+        }
+    };
+    request.send();    
+};
+L.Util.jsonp = function (url, cb, cbParam, callbackName){
+    var cbn,ourl,cbs;
+    var cbParam = cbParam || "callback";
+    if(callbackName){
+        cbn= callbackName;
+    }else{
+        cbs = "_" + Math.floor(Math.random()*1000000);
+        cbn = "L.Util.jsonp.cb." + cbs;
+    }
+    L.Util.jsonp.cb = {};
+    L.Util.jsonp.cb[cbs] = cb;
+    var scriptNode = L.DomUtil.create('script','', document.getElementsByTagName('body')[0] );
+    scriptNode.type = 'text/javascript';
+    if (url.indexOf("?") === -1 ){
+        ourl =  url+"?"+cbParam+"="+cbn;
+    }else{
+        ourl =  url+"&"+cbParam+"=."+cbn;
+    }
+    scriptNode.src = ourl;   
+};
 L.geoJson.ajax = function (geojson, options) {
     return new L.GeoJSON.AJAX(geojson, options);
 };
