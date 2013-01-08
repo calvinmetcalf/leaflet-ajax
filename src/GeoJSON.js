fa81f8833cc5326608209ce0d68c18fa07112422
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-01-08 15:18:50 -0500

    updates

--- src/GeoJSON.js
+++ src/GeoJSON.js
@@ -1,7 +1,8 @@
 L.GeoJSON.AJAX=L.GeoJSON.extend({
     defaultAJAXparams:{
      dataType:"json",
-     callbackParam:"callback"
+     callbackParam:"callback",
+     middleware:function(f){return f;}
     },
     initialize: function (url, options) { // (String, Object)
 
@@ -9,11 +10,11 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
         var ajaxParams = L.Util.extend({}, this.defaultAJAXparams);
 
         for (var i in options) {
-    		if (this.defaultAJAXparams.hasOwnProperty(i)) {
+            if (this.defaultAJAXparams.hasOwnProperty(i)) {
 				ajaxParams[i] = options[i];
 			}
 		}
-
+		this._cache=[]
 		this.ajaxParams = ajaxParams;
         this._layers = {};
 		L.Util.setOptions(this, options);
@@ -24,10 +25,11 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
     addUrl: function (url) {
         var _this = this;
         _this._url = url;
+        
         if(this.ajaxParams.dataType.toLowerCase()==="json"){
-          L.Util.ajax(url, function(data){_this._cache=data;_this.addData(data);}); 
+          L.Util.ajax(url, function(d){var data = _this.ajaxParams.middleware(d);_this._cache=_this._cache.concat(data.features);_this.addData(data);_this.fire("dataLoaded");}); 
         }else if(this.ajaxParams.dataType.toLowerCase()==="jsonp"){
-            L.Util.jsonp(url, function(data){_this._cache=data;_this.addData(data);}, _this.ajaxParams.callbackParam);
+            L.Util.jsonp(url, function(d){var data = _this.ajaxParams.middleware(d);_this._cache=_this._cache.concat(data.features);;_this.addData(data);_this.fire("dataLoaded");}, _this.ajaxParams.callbackParam);
         }
     },
     refresh: function (url){
@@ -40,7 +42,7 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
             func = function(){return true;};
         }
         this.clearLayers();
-        this.addData(this._cache.features.filter(func));
+        this.addData(this._cache.filter(func));
     }
 });
 L.geoJson.ajax = function (geojson, options) {
