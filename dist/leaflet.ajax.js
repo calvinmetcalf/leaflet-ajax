039d040986bde7e0eb0b23359b6e3aa37e191d86
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-03-14 14:57:09 -0400

    updated build

--- dist/leaflet.ajax.js
+++ dist/leaflet.ajax.js
@@ -28,16 +28,18 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
 		this._layers = {};
 		L.Util.setOptions(this, options);
 		if(this._urls.length > 0){
-			this.addUrl(this._urls);
+			this.addUrl();
 		}
 	},
 	addUrl: function (url) {
 		var _this = this;
-		if (typeof url === "string") {
+		if(url){
+			if (typeof url === "string") {
 				_this._urls.push(url);
 			}else if (typeof url.pop === "function") {
 				_this._urls = _this._urls.concat(url)
 			}
+		}
 		var _this = this;
 		var len = _this._urls.length;
 		var i=0;
@@ -46,7 +48,7 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
 			if(_this.ajaxParams.dataType.toLowerCase()==="json"){	
 			  L.Util.ajax(_this._urls[i], function(d){var data = _this.ajaxParams.middleware(d);_this.addData(data);_this.fire("dataLoaded");}); 
 			}else if(_this.ajaxParams.dataType.toLowerCase()==="jsonp"){
-				L.Util.jsonp(_this._urls[i], function(d){var data = _this.ajaxParams.middleware(d);_this.addData(data);_this.fire("dataLoaded");}, _this.ajaxParams.callbackParam);
+				L.Util.ajax(_this._urls[i],{jsonp:true}, function(d){var data = _this.ajaxParams.middleware(d);_this.addData(data);_this.fire("dataLoaded");}, _this.ajaxParams.callbackParam);
 			}
 			i++
 		}
@@ -76,61 +78,66 @@ L.GeoJSON.AJAX=L.GeoJSON.extend({
 L.geoJson.ajax = function (geojson, options) {
 	return new L.GeoJSON.AJAX(geojson, options);
 };
-L.Util.ajax = function (url, cb) {
-	// the following is from JavaScript: The Definitive Guide
-	if (window.XMLHttpRequest === undefined) {
-		window.XMLHttpRequest = function() {
-			try {
-				return new ActiveXObject("Microsoft.XMLHTTP.6.0");
-			}
-			catch  (e1) {
-				try {
-					return new ActiveXObject("Microsoft.XMLHTTP.3.0");
-				}
-				catch (e2) {
-					throw new Error("XMLHttpRequest is not supported");
-				}
-			}
-		};
+L.Util.ajax = function (url,options, cb) {
+	var cbName,ourl,cbSuffix,scriptNode, head, cbParam;
+	if(typeof options === "function"){
+		cb = options;
+		options = {};
 	}
-	var response, request = new XMLHttpRequest();
-	request.open("GET", url);
-	request.onreadystatechange = function() {
-		if (request.readyState === 4 && request.status === 200) {
-			if(window.JSON) {
-				response = JSON.parse(request.responseText);
-			} else {
-				response = eval("("+ request.responseText + ")");
-			}
-			cb(response);
+	if(options.jsonp){
+		head = document.getElementsByTagName('head')[0];
+		cbParam = options.cbParam || "callback";
+		if(options.callbackName){
+			cbName= options.callbackName;
+		}else{
+			cbSuffix = "_" + ("" + Math.random()).slice(2);
+			cbName = "L.Util.ajax.cb." + cbSuffix;
 		}
-	};
-	request.send();	
-};
-L.Util.jsonp = function (url, cb, cbParam, callbackName){
-	var cbName,ourl,cbSuffix,scriptNode,
-	   head = document.getElementsByTagName('head')[0];
-	var cbParam = cbParam || "callback";
-	if(callbackName){
-		cbName= callbackName;
-	}else{
-		cbSuffix = "_" + ("" + Math.random()).slice(2);
-		cbName = "L.Util.jsonp.cb." + cbSuffix;
-	}
-	scriptNode = L.DomUtil.create('script', '', head);
-	scriptNode.type = 'text/javascript';
-	if(cbSuffix) {
-		L.Util.jsonp.cb[cbSuffix] = function(data){
-			head.removeChild(scriptNode);
-			delete L.Util.jsonp.cb[cbSuffix]
-			cb(data);
+		scriptNode = L.DomUtil.create('script', '', head);
+		scriptNode.type = 'text/javascript';
+		if(cbSuffix) {
+			L.Util.ajax.cb[cbSuffix] = function(data){
+				head.removeChild(scriptNode);
+				delete L.Util.ajax.cb[cbSuffix]
+				cb(data);
+			};
+		}
+		if (url.indexOf("?") === -1 ){
+			ourl =  url+"?"+cbParam+"="+cbName;
+		}else{
+			ourl =  url+"&"+cbParam+"="+cbName;
+		}
+		scriptNode.src = ourl;
+	}else{	
+		// the following is from JavaScript: The Definitive Guide
+		if (window.XMLHttpRequest === undefined) {
+			window.XMLHttpRequest = function() {
+				try {
+					return new ActiveXObject("Microsoft.XMLHTTP.6.0");
+				}
+				catch  (e1) {
+					try {
+						return new ActiveXObject("Microsoft.XMLHTTP.3.0");
+					}
+					catch (e2) {
+						throw new Error("XMLHttpRequest is not supported");
+					}
+				}
+			};
+		}
+		var response, request = new XMLHttpRequest();
+		request.open("GET", url);
+		request.onreadystatechange = function() {
+			if (request.readyState === 4 && request.status === 200) {
+				if(window.JSON) {
+					response = JSON.parse(request.responseText);
+				} else {
+					response = eval("("+ request.responseText + ")");
+				}
+				cb(response);
+			}
 		};
+		request.send();	
 	}
-	if (url.indexOf("?") === -1 ){
-		ourl =  url+"?"+cbParam+"="+cbName;
-	}else{
-		ourl =  url+"&"+cbParam+"="+cbName;
-	}
-	scriptNode.src = ourl;   
 };
-L.Util.jsonp.cb = {};
+L.Util.ajax.cb = {};
